f678b52e5d78b3b53bc9e268440bb52977d66c2a
Ayato233 <2783885223@qq.com>
2025-03-18 17:16:37 +0800

    Site updated: 2025-03-18 17:16:37

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/18/hello-world/"];function toRandomPost(){
+var posts=["2025/03/18/还没发工资/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
