9ce55f5b5f8e9143eba71c18ccbeb1191965eee8
Ayato233 <2783885223@qq.com>
2025-03-23 17:56:02 +0800

    Site updated: 2025-03-23 17:56:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2013/12/24/hello-world/"];function toRandomPost(){
+var posts=["2025/03/23/MyBlog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
