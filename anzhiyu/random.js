db11bf8015ce3eefb4a33d105a1b7d7b8b44664e
Ayato233 <2783885223@qq.com>
2025-03-24 16:31:27 +0800

    Site updated: 2025-03-24 16:31:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/23/MyBlog/"];function toRandomPost(){
+var posts=["2025/03/23/遗憾/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
