1f649c8f0a37d1b163539e5dd32ba4e6627f5758
dev <dev@local>
2026-08-06 06:00:07 +0800

    config: 移除 My 菜单中的 Devices 子项

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -181,12 +181,6 @@ export const navBarConfig: NavBarConfig = {
 					url: "/albums/",
 					icon: "material-symbols:photo-library",
 				},
-				{
-					name: "Devices",
-					url: "/devices/",
-					icon: "material-symbols:devices",
-					external: false, // 内部链接，当前页导航
-				},
 			],
 		},
 
