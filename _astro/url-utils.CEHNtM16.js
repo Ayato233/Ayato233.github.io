4a4b15310772f7dce48f6398560944fdeac12adf
Ayato233 <2783885223@qq.com>
2026-08-19 23:47:11 +0000

    Deploying to pages from @ Ayato233/Ayato233.github.io@a27a8b24d48e60e9fb1ea7ead770aa7283365deb 🚀

---
+++ _astro/url-utils.CEHNtM16.js
@@ -0,0 +1,1 @@
+import"./translation.BTLALlnL.js";import"./config.BAMD2xNg.js";function a(r,e){return r.replace(/^\/|\/$/g,"").toLowerCase()===e.replace(/^\/|\/$/g,"").toLowerCase()}function t(...r){return r.join("/").replace(/\/+/g,"/")}function u(r){return t("","/",r)}export{u as n,a as t};
